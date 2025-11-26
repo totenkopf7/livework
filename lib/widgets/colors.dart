27b91230af5f7e09177dbbc3d6269ff8419aa5af
totenkopf7 <kurdishlearner2018@gmail.com>
2025-11-26 23:22:53 +0300

    Changed the layout color - 26 Nov

--- lib/widgets/colors.dart
+++ lib/widgets/colors.dart
@@ -5,7 +5,8 @@ class AppColors {
   // static const Color secondary = Color(0xFFfca311); // Gold-ish
   // static const Color background = Color.fromARGB(255, 31, 30, 30);
   // static const Color secondary = Color.fromARGB(255, 233, 232, 230);
-    static const Color background = Color.fromARGB(255, 31, 30, 30);
+  // static const Color background = Color.fromARGB(255, 31, 30, 30);
+  static const Color background = Color(0xFF0B335F);
   static const Color secondary = Color.fromARGB(255, 233, 232, 230);
   static const Color text = Color(0xFF001F3F);
   static const Color warmBeige = Color(0xFFD8CBB7); // Warm Beige
